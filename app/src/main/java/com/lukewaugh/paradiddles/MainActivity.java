fddab2f469844874abe963333da819fc34457fc5
lukeyy33 <b3030652@my.shu.ac.uk>
2016-03-07 16:10:00 +0000

    Created 2 buttons, "Previous" & "Next" which cycle through images

--- app/src/main/java/com/lukewaugh/paradiddles/MainActivity.java
+++ app/src/main/java/com/lukewaugh/paradiddles/MainActivity.java
@@ -1,22 +1,38 @@
 package com.lukewaugh.paradiddles;
 
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button nextBtn, previousBtn;
+    private ViewFlipper viewFlipper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        nextBtn = (Button)findViewById(R.id.nextBtn);
+        previousBtn = (Button) findViewById(R.id.previousBtn);
+        viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+
+        nextBtn.setOnClickListener(this);
+        previousBtn.setOnClickListener(this);
+
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,5 +54,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == nextBtn)
+            viewFlipper.showNext();
+        else if (v == previousBtn)
+            viewFlipper.showPrevious();
+
     }
 }
