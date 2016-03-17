ea2250cc8976de0d054fb7939008818ed301020d
lukeyy33 <b3030652@my.shu.ac.uk>
2016-03-17 10:00:20 +0000

    Added 5 layouts for respective paradiddles with home button to return to home screen. Added onBackPressed method()

--- app/src/main/java/com/lukewaugh/paradiddles/MainActivity.java
+++ app/src/main/java/com/lukewaugh/paradiddles/MainActivity.java
@@ -1,36 +1,58 @@
 package com.lukewaugh.paradiddles;
 
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private Button nextBtn, previousBtn;
-    private ViewFlipper viewFlipper;
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nextBtn = (Button)findViewById(R.id.nextBtn);
-        previousBtn = (Button) findViewById(R.id.previousBtn);
-        viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+        Button nextBtn, previousBtn, homeBtn;
+        ViewFlipper viewFlipper;
+        Button singlesBtn, doublesBtn, tripletsBtn,
+                flamsBtn, dragsBtn, rollsBtn;
 
-        nextBtn.setOnClickListener(this);
-        previousBtn.setOnClickListener(this);
+        //Buttons
+        nextBtn     = (Button) findViewById(R.id.nextBtn);
+        previousBtn = (Button) findViewById(R.id.previousBtn);
+        singlesBtn  = (Button) findViewById(R.id.btnSingles);
+        doublesBtn  = (Button) findViewById(R.id.btnDoubles);
+        tripletsBtn = (Button) findViewById(R.id.btnTriplets);
+        flamsBtn    = (Button) findViewById(R.id.btnFlams);
+        dragsBtn    = (Button) findViewById(R.id.btnDrags);
+        rollsBtn    = (Button) findViewById(R.id.btnRolls);
+        //homeBtn     = (Button) findViewById(R.id.btnHome);
+
+
+        //viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+
+        findViewById(R.id.nextBtn).setOnClickListener(handler);
+        findViewById(R.id.previousBtn).setOnClickListener(handler);
+
+        findViewById(R.id.btnSingles).setOnClickListener(handler);
+        findViewById(R.id.btnDoubles).setOnClickListener(handler);
+
+        findViewById(R.id.btnTriplets).setOnClickListener(handler);
+        findViewById(R.id.btnFlams).setOnClickListener(handler);
+
+        findViewById(R.id.btnDrags).setOnClickListener(handler);
+        findViewById(R.id.btnRolls).setOnClickListener(handler);
+
+        //findViewById(R.id.btnHome).setOnClickListener(handler);
+
 
     }
-
 
 
 
@@ -56,12 +78,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v == nextBtn)
-            viewFlipper.showNext();
-        else if (v == previousBtn)
-            viewFlipper.showPrevious();
 
+    View.OnClickListener handler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.nextBtn:
+                    //viewFlipper.showNext();
+                    break;
+                case R.id.previousBtn:
+                    //viewFlipper.showPrevious();
+                    break;
+                case R.id.btnSingles:
+                    setContentView(R.layout.singles_main);
+                    break;
+                case R.id.btnDoubles:
+                    setContentView(R.layout.doubles_main);
+                    break;
+                case R.id.btnHome:
+                    setContentView(R.layout.activity_main);
+                    break;
+            }
+
+        }
+    };
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(MainActivity.this, MainActivity.class));
+        finish();
     }
+
 }
