ea9f63fbad8483570ac1ae036e8f291afe322a4e
lukeyy33 <b3030652@my.shu.ac.uk>
2016-03-23 19:41:36 +0000

    Changed methods for onClick listeners

--- app/src/main/java/com/lukewaugh/paradiddles/MainActivity.java
+++ app/src/main/java/com/lukewaugh/paradiddles/MainActivity.java
@@ -13,11 +13,10 @@ import android.view.View;
 import android.widget.Button;
 
 import com.lukewaugh.paradiddles.authClasses.AccountActivity;
-import com.lukewaugh.paradiddles.authClasses.LoginActivity;
 
 @SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity implements
-        NavigationView.OnNavigationItemSelectedListener {
+        NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,55 +37,23 @@ public class MainActivity extends AppCompatActivity implements
         navigationView.setNavigationItemSelectedListener(this);
 
         Button singlesBtn  = (Button) findViewById(R.id.btnSingles);
-        singlesBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), SinglesActivity.class);
-                startActivityForResult(intent, 0);
+        singlesBtn.setOnClickListener(this);
 
-            }
-        });
         Button doublesBtn  = (Button) findViewById(R.id.btnDoubles);
-        doublesBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), DoublesActivity.class);
-                startActivityForResult(intent, 0);
+        doublesBtn.setOnClickListener(this);
 
-            }
-        });
         Button tripletsBtn = (Button) findViewById(R.id.btnTriplets);
-        tripletsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), TripletsActivity.class);
-                startActivityForResult(intent, 0);
-            }
-        });
+        tripletsBtn.setOnClickListener(this);
+
         Button flamsBtn = (Button) findViewById(R.id.btnFlams);
-        flamsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), FlamsActivity.class);
-                startActivityForResult(intent, 0);
-            }
-        });
+        flamsBtn.setOnClickListener(this);
+
         Button dragsBtn = (Button) findViewById(R.id.btnDrags);
-        dragsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), DragsActivity.class);
-                startActivityForResult(intent, 0);
-            }
-        });
-        Button rollsBtn    = (Button) findViewById(R.id.btnRolls);
-        rollsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), RollsActivity.class);
-                startActivityForResult(intent, 0);
-            }
-        });
+        dragsBtn.setOnClickListener(this);
+
+        Button rollsBtn = (Button) findViewById(R.id.btnRolls);
+        rollsBtn.setOnClickListener(this);
+
 
     }
     @Override
@@ -99,25 +66,66 @@ public class MainActivity extends AppCompatActivity implements
         }
     }
 
+//    @Override
+//    public boolean onNavigationItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        if (id == R.id.settings) {
+//            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+//            startActivity(intent);
+//
+//        } else if (id == R.id.account) {
+//            Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+//            startActivity(intent);
+//
+//        } else if (id == R.id.home) {
+//            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+//            startActivity(intent);
+//        }
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+//        drawer.closeDrawer(GravityCompat.START);
+//        return true;
+//    }
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         int id = item.getItemId();
-
-        if (id == R.id.settings) {
-            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-            startActivity(intent);
-
-        } else if (id == R.id.account) {
-            Intent intent = new Intent(MainActivity.this, AccountActivity.class);
-            startActivity(intent);
-
-        } else if (id == R.id.home) {
-            Intent intent = new Intent(MainActivity.this, MainActivity.class);
-            startActivity(intent);
+        switch (id) {
+            case R.id.settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                break;
+            case R.id.account:
+                startActivity(new Intent(this, AccountActivity.class));
+                break;
+            case R.id.home:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+        return true;
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnSingles:
+                startActivity(new Intent(this, SinglesActivity.class));
+                break;
+            case R.id.btnDoubles:
+                startActivity(new Intent(this, DoublesActivity.class));
+                break;
+            case R.id.btnTriplets:
+                startActivity(new Intent(this, TripletsActivity.class));
+                break;
+            case R.id.btnDrags:
+                startActivity(new Intent(this, DragsActivity.class));
+                break;
+            case R.id.btnFlams:
+                startActivity(new Intent(this, FlamsActivity.class));
+                break;
+            case R.id.btnRolls:
+                startActivity(new Intent(this, RollsActivity.class));
+                break;
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
